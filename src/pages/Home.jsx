c2137cba287fdf2a0cdfde49a7256bfb3d57bca1
Lucas Filgueiras <lucasfilgueiras@rocketmail.com>
2022-05-19 14:34:21 -0300

    Feedback list, item and form using context now

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,14 @@
-import {v4 as uuidv4} from 'uuid'
-import { useState } from "react"
-
-import FeedbackData from "../data/FeedbackData"
 import FeedbackList from "../components/FeedbackList"
 import FeedbackStats from "../components/FeedbackStats"
 import FeedbackForm from "../components/FeedbackForm"
 import {FeedbackProvider} from "../context/FeedbackContext"
 
 function Home() {
-    const [feedback, setFeedback] = useState(FeedbackData)
-
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
-    }
-
-    const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you want to delete?')){
-        setFeedback(feedback.filter((item) => item.id !== id))
-        }
-    }
-
   return (
     <FeedbackProvider>
-        <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackForm />
         <FeedbackStats />
-        <FeedbackList handleDelete={deleteFeedback}/>
+        <FeedbackList />
     </FeedbackProvider>
   )
 }
