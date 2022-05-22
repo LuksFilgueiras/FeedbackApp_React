809b524e65d9ac5979c81c292dc73901a1169a1b
Lucas Filgueiras <lucasfilgueiras@rocketmail.com>
2022-05-22 16:09:10 -0300

    Fetching data from backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,32 +1,27 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import {v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This item 1 from context',
-            rating: 10,
-        },
-        {
-            id: 2,
-            text: 'This item 2 from context',
-            rating: 8,
-        },
-        {
-            id: 3,
-            text: 'This item 3 from context',
-            rating: 5,
-        }
-    ])
+    const [feedback, setFeedback] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() =>{
+        fetchFeedback()
+    }, [])
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
 
+    const fetchFeedback = async () =>{
+        const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc")
+        const data = await response.json()
+        setFeedback(data)
+        setIsLoading(false)
+    }
     // Set item to be updated
     const editFeedback = (item) =>{
         setFeedbackEdit({
@@ -59,6 +54,7 @@ export const FeedbackProvider = ({children}) => {
     value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
